4d69a5e187451cf425fd9da6cd2f5b6d18e86952
raptor <raptor@0xdeadbeef.info>
2025-04-22 18:26:35 +0200

    feat: part-02, add custom target specification and build the `core` library

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@
 
 use core::panic::PanicInfo;
 
-/// Program's entry point
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
-
 /// Panic handler
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// Program's entry point
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
