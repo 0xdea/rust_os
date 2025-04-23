d2e33c5027b4c4f9ede2e65e739a54caf4b27b28
raptor <raptor@0xdeadbeef.info>
2025-04-23 10:03:18 +0200

    feat: part-02, add code to print a string in `_start`

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@
 
 use core::panic::PanicInfo;
 
+/// String to print
+static HELLO: &[u8] = b"Hello World!";
+
+/// Color code for cyan
+const COLOR_CYAN: u8 = 0x0b;
+
 /// Panic handler
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,5 +18,14 @@ fn panic(_info: &PanicInfo) -> ! {
 /// Program's entry point
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = COLOR_CYAN;
+        }
+    }
+
     loop {}
 }
