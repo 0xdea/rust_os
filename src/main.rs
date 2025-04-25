75576ff3c26795bea07df5b22b9a25bb909618bb
raptor <raptor@0xdeadbeef.info>
2025-04-25 15:46:54 +0200

    feat: part-04, add a custom test framework

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,9 @@
 
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -36,8 +39,19 @@ fn panic(info: &PanicInfo) -> ! {
 #[allow(clippy::missing_panics_doc)] // Writes to the VGA buffer never fail
 extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    // panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
 
     #[allow(clippy::empty_loop)]
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    println!("Ok");
 }
