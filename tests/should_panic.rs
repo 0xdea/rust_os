d0b986ba6c0bd9dca6a5fe153091d9231bcb6ccf
raptor <raptor@0xdeadbeef.info>
2025-05-02 21:55:33 +0200

    test: part-04, simplify should_panic.rs integration test by removing the test harness

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -2,13 +2,10 @@
 
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
-use rust_os::{QemuExitCode, exit_qemu, serial_println};
+use rust_os::{QemuExitCode, exit_qemu, serial_print, serial_println};
 
 /// Panic handler
 #[panic_handler]
@@ -19,35 +16,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// Custom test runner for this integration test
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failure);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
 /// Integration test entry point
 //noinspection RsUnresolvedPath
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failure);
 
     #[allow(clippy::empty_loop)]
     loop {}
 }
 
-// Only a single test case is supported
-#[cfg(test)]
-mod tests {
-    use rust_os::serial_print;
-
-    #[test_case]
-    fn should_fail() {
-        serial_print!("should_panic::should_fail...\t");
-        assert_eq!(0, 1);
-    }
+fn should_fail() {
+    serial_print!("should_panic::should_fail... ");
+    assert_eq!(0, 1);
 }
