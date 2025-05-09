aad510ec8205e0cfedc1c2d9f48056f8be1159d9
raptor <raptor@0xdeadbeef.info>
2025-05-09 09:42:36 +0200

    style: post-06, rearrange comments

--- src/gdt.rs
+++ src/gdt.rs
@@ -13,14 +13,14 @@ lazy_static! {
         // Create the TSS
         let mut tss = TaskStateSegment::new();
 
-        // Use a static mut array as stack storage
         // TODO: Use a proper stack allocator and add a stack guard
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
             const STACK_SIZE: usize = 4096 * 5;
+            // Use a static mut array as stack storage
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
             let stack_start = VirtAddr::from_ptr(&raw const STACK);
-            
+
             // Return the top of the stack
             stack_start + STACK_SIZE.try_into().unwrap()
         };
