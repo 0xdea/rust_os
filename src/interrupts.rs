89ad8f2e2e75281b2fc297b640db41ac026f79cb
raptor <raptor@0xdeadbeef.info>
2025-05-13 17:35:18 +0200

    feat: post-07, add a timer interrupt handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -4,30 +4,7 @@ use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-use crate::{gdt, println};
-
-lazy_static! {
-    /// IDT
-    static ref IDT: InterruptDescriptorTable = {
-        // Create the IDT
-        let mut idt = InterruptDescriptorTable::new();
-
-        // Set handler functions
-        unsafe {
-            // Use a dedicated stack for the double fault handler
-            idt.double_fault.set_handler_fn(double_fault_handler)
-                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
-        }
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
-
-        idt
-    };
-}
-
-/// Load the IDT in the CPU
-pub fn init_idt() {
-    IDT.load();
-}
+use crate::{gdt, print, println};
 
 /// PIC1 interrupt offset
 const PIC1_OFFSET: u8 = 32;
@@ -44,6 +21,45 @@ pub fn init_pics() {
     unsafe { PICS.lock().initialize() };
 }
 
+/// Interrupt variants
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    /// Timer interrupt
+    Timer = PIC1_OFFSET,
+}
+
+impl InterruptIndex {
+    /// Return `InterruptIndex` as a `u8`
+    const fn as_u8(self) -> u8 {
+        self as u8
+    }
+}
+
+lazy_static! {
+    /// IDT
+    static ref IDT: InterruptDescriptorTable = {
+        // Create the IDT
+        let mut idt = InterruptDescriptorTable::new();
+
+        // Set handler functions
+        unsafe {
+            // Use a dedicated stack for the double fault handler
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt[InterruptIndex::Timer.as_u8()].set_handler_fn(timer_interrupt_handler);
+
+        idt
+    };
+}
+
+/// Load the IDT in the CPU
+pub fn init_idt() {
+    IDT.load();
+}
+
 /// Double fault handler
 extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame,
@@ -55,6 +71,11 @@ extern "x86-interrupt" fn double_fault_handler(
 /// Breakpoint exception handler
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+/// Timer interrupt handler
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    print!(".");
 }
 
 #[cfg(test)]
