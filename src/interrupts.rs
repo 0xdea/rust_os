bf9d3ba8a15ffce7cc7eef4f7b57fdbe64a640b9
raptor <raptor@0xdeadbeef.info>
2025-05-15 14:52:58 +0200

    feat: post-06, translate scancodes into keys

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -89,9 +89,25 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
 
 /// Keyboard interrupt handler
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    let mut port = x86_64::instructions::port::Port::<u8>::new(0x60);
-    let scancode = unsafe { port.read() };
-    print!("{scancode}");
+    const KEYBOARD_DATA_PORT: u16 = 0x60;
+    let mut port = x86_64::instructions::port::Port::<u8>::new(KEYBOARD_DATA_PORT);
+
+    let key = match unsafe { port.read() } {
+        0x02 => Some('1'),
+        0x03 => Some('2'),
+        0x04 => Some('3'),
+        0x05 => Some('4'),
+        0x06 => Some('5'),
+        0x07 => Some('6'),
+        0x08 => Some('7'),
+        0x09 => Some('8'),
+        0x0a => Some('9'),
+        0x0b => Some('0'),
+        _ => None,
+    };
+    if let Some(key) = key {
+        print!("{key}");
+    }
 
     unsafe {
         PICS.lock()
