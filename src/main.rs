25ff0fd6fe1196c5dba6a79c4610fef3d5994657
raptor <raptor@0xdeadbeef.info>
2025-05-26 17:08:03 +0200

    feat: post-09, access the active level 3 table

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use bootloader::{BootInfo, entry_point};
 use rust_os::memory::active_level_4_table;
 use rust_os::{hlt_loop, println};
 use x86_64::VirtAddr;
+use x86_64::structures::paging::PageTable;
 
 /// Panic handler
 #[cfg(not(test))]
@@ -48,6 +49,19 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     for (i, entry) in l4_table.iter().enumerate() {
         if !entry.is_unused() {
             println!("L4 Entry {i}: {entry:?}");
+
+            // Get the physical address from the entry and convert it
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            // Print non-empty entries of the level 3 table
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("  L3 Entry {}: {:?}", i, entry);
+                }
+            }
         }
     }
 
