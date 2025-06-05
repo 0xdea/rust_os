44e2655d5eb13adf2df261ab11e119ff0243495a
raptor <raptor@0xdeadbeef.info>
2025-06-05 16:13:02 +0200

    feat: post-10, use a proper allocator crate to fix the out of memory error

--- src/allocator.rs
+++ src/allocator.rs
@@ -3,30 +3,18 @@
 use core::alloc::GlobalAlloc;
 use core::ptr::null_mut;
 
+use linked_list_allocator::LockedHeap;
 use x86_64::VirtAddr;
 use x86_64::structures::paging::mapper::MapToError;
 use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB};
 
 /// Memory address where the heap starts
-pub const HEAP_START: u64 = 0x0000_4444_4444_0000;
+pub const HEAP_START: usize = 0x0000_4444_4444_0000;
 /// Size of the heap in bytes
-pub const HEAP_SIZE: u64 = 100 * 1024; // 100 KiB
+pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
 #[global_allocator]
-static ALLOCATOR: Dummy = Dummy;
-
-/// Dummy allocator
-pub struct Dummy;
-
-unsafe impl GlobalAlloc for Dummy {
-    unsafe fn alloc(&self, _layout: core::alloc::Layout) -> *mut u8 {
-        null_mut()
-    }
-
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: core::alloc::Layout) {
-        panic!("Not implemented");
-    }
-}
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 /// Initialize the heap based on a [`Mapper`] and a [`FrameAllocator`] instance (both limited to
 /// 4 KiB pages).
@@ -40,8 +28,8 @@ pub fn init_heap(
 ) -> Result<(), MapToError<Size4KiB>> {
     // Create the page range
     let page_range = {
-        let heap_start = VirtAddr::new(HEAP_START);
-        let heap_end = heap_start + HEAP_SIZE - 1;
+        let heap_start = VirtAddr::new(HEAP_START as u64);
+        let heap_end = heap_start + HEAP_SIZE as u64 - 1;
 
         let heap_start_page = Page::containing_address(heap_start);
         let heap_end_page = Page::containing_address(heap_end);
@@ -49,7 +37,7 @@ pub fn init_heap(
         Page::range_inclusive(heap_start_page, heap_end_page)
     };
 
-    // Map the pages in the range
+    // Map all heap pages to physical frames
     for page in page_range {
         let frame = frame_allocator
             .allocate_frame()
@@ -57,6 +45,11 @@ pub fn init_heap(
         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
 
         unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() };
+    }
+
+    // Initialize the allocator
+    unsafe {
+        ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
     }
 
     Ok(())
