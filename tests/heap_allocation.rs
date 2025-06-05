237ad1a812cc6f1e927bd02a96e1e2f32b6dacd4
raptor <raptor@0xdeadbeef.info>
2025-06-05 16:54:37 +0200

    test: post-10, add more test cases

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -40,6 +40,9 @@ fn main(boot_info: &'static BootInfo) -> ! {
 #[cfg(test)]
 mod tests {
     use alloc::boxed::Box;
+    use alloc::vec::Vec;
+
+    use rust_os::allocator::HEAP_SIZE;
 
     #[test_case]
     fn simple_allocation() {
@@ -47,5 +50,23 @@ mod tests {
         let heap_value_2 = Box::new(13);
         assert_eq!(*heap_value_1, 41);
         assert_eq!(*heap_value_2, 13);
+    }
+
+    #[test_case]
+    fn large_vec() {
+        let n = 1000;
+        let mut vec = Vec::new();
+        for i in 0..n {
+            vec.push(i);
+        }
+        assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
+    }
+
+    #[test_case]
+    fn many_boxes() {
+        for i in 0..HEAP_SIZE {
+            let x = Box::new(i);
+            assert_eq!(*x, i);
+        }
     }
 }
